ef14e15a9720f9025dea816f1f52b5b9b831e7af
harley santos garzon <harley.santos.garzon@gmail.com>
2022-02-24 23:11:35 -0500

    feat(brand) new component

---
+++ components/brand.js
@@ -0,0 +1,162 @@
+
+export default function Brand () {
+  return (
+    <figure style={{margin: 0}}>
+      <SvgComponent />
+    </figure>
+  )
+}
+
+
+const SvgComponent = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width={49}
+    height={67}
+    {...props}
+  >
+    <title>{"Sheets-icon"}</title>
+    <defs>
+      <path
+        d="M29.583 0H4.438A4.45 4.45 0 0 0 0 4.438v56.208a4.45 4.45 0 0 0 4.438 4.437h38.458a4.45 4.45 0 0 0 4.437-4.437V17.75L29.583 0Z"
+        id="a"
+      />
+      <path
+        d="M29.583 0H4.438A4.45 4.45 0 0 0 0 4.438v56.208a4.45 4.45 0 0 0 4.438 4.437h38.458a4.45 4.45 0 0 0 4.437-4.437V17.75L29.583 0Z"
+        id="c"
+      />
+      <path
+        d="M29.583 0H4.438A4.45 4.45 0 0 0 0 4.438v56.208a4.45 4.45 0 0 0 4.438 4.437h38.458a4.45 4.45 0 0 0 4.437-4.437V17.75L29.583 0Z"
+        id="e"
+      />
+      <path
+        d="M29.583 0H4.438A4.45 4.45 0 0 0 0 4.438v56.208a4.45 4.45 0 0 0 4.438 4.437h38.458a4.45 4.45 0 0 0 4.437-4.437V17.75L29.583 0Z"
+        id="h"
+      />
+      <path
+        d="M29.583 0H4.438A4.45 4.45 0 0 0 0 4.438v56.208a4.45 4.45 0 0 0 4.438 4.437h38.458a4.45 4.45 0 0 0 4.437-4.437V17.75L29.583 0Z"
+        id="j"
+      />
+      <path
+        d="M29.583 0H4.438A4.45 4.45 0 0 0 0 4.438v56.208a4.45 4.45 0 0 0 4.438 4.437h38.458a4.45 4.45 0 0 0 4.437-4.437V17.75L29.583 0Z"
+        id="l"
+      />
+      <path
+        d="M29.583 0H4.438A4.45 4.45 0 0 0 0 4.438v56.208a4.45 4.45 0 0 0 4.438 4.437h38.458a4.45 4.45 0 0 0 4.437-4.437V17.75L29.583 0Z"
+        id="n"
+      />
+      <radialGradient
+        cx="3.168%"
+        cy="2.717%"
+        fx="3.168%"
+        fy="2.717%"
+        r="161.249%"
+        gradientTransform="matrix(1 0 0 .72727 0 .007)"
+        id="p"
+      >
+        <stop stopColor="#FFF" stopOpacity={0.1} offset="0%" />
+        <stop stopColor="#FFF" stopOpacity={0} offset="100%" />
+      </radialGradient>
+      <linearGradient
+        x1="50.005%"
+        y1="8.586%"
+        x2="50.005%"
+        y2="100.014%"
+        id="f"
+      >
+        <stop stopColor="#263238" stopOpacity={0.2} offset="0%" />
+        <stop stopColor="#263238" stopOpacity={0.02} offset="100%" />
+      </linearGradient>
+    </defs>
+    <g fill="none" fillRule="evenodd">
+      <g transform="translate(.833 .958)">
+        <mask id="b" fill="#fff">
+          <use xlinkHref="#a" />
+        </mask>
+        <path
+          d="M29.583 0H4.438A4.45 4.45 0 0 0 0 4.438v56.208a4.45 4.45 0 0 0 4.438 4.437h38.458a4.45 4.45 0 0 0 4.437-4.437V17.75L36.98 10.354 29.583 0Z"
+          fill="#0F9D58"
+          fillRule="nonzero"
+          mask="url(#b)"
+        />
+      </g>
+      <g transform="translate(.833 .958)">
+        <mask id="d" fill="#fff">
+          <use xlinkHref="#c" />
+        </mask>
+        <path
+          d="M11.833 31.802V53.25H35.5V31.802H11.833Zm10.354 18.49h-7.395v-3.698h7.396v3.698Zm0-5.917h-7.395v-3.698h7.396v3.698Zm0-5.917h-7.395V34.76h7.396v3.698Zm10.355 11.834h-7.396v-3.698h7.396v3.698Zm0-5.917h-7.396v-3.698h7.396v3.698Zm0-5.917h-7.396V34.76h7.396v3.698Z"
+          fill="#F1F1F1"
+          fillRule="nonzero"
+          mask="url(#d)"
+        />
+      </g>
+      <g transform="translate(.833 .958)">
+        <mask id="g" fill="#fff">
+          <use xlinkHref="#e" />
+        </mask>
+        <path
+          fill="url(#f)"
+          fillRule="nonzero"
+          mask="url(#g)"
+          d="M30.881 16.452 47.333 32.9V17.75z"
+        />
+      </g>
+      <g transform="translate(.833 .958)">
+        <mask id="i" fill="#fff">
+          <use xlinkHref="#h" />
+        </mask>
+        <g mask="url(#i)">
+          <path
+            d="M29.583 0v13.313a4.436 4.436 0 0 0 4.438 4.437h13.312L29.583 0Z"
+            fill="#87CEAC"
+            fillRule="nonzero"
+          />
+        </g>
+      </g>
+      <g transform="translate(.833 .958)">
+        <mask id="k" fill="#fff">
+          <use xlinkHref="#j" />
+        </mask>
+        <path
+          d="M4.438 0A4.45 4.45 0 0 0 0 4.438v.37A4.45 4.45 0 0 1 4.438.37h25.145V0H4.438Z"
+          fillOpacity={0.2}
+          fill="#FFF"
+          fillRule="nonzero"
+          mask="url(#k)"
+        />
+      </g>
+      <g transform="translate(.833 .958)">
+        <mask id="m" fill="#fff">
+          <use xlinkHref="#l" />
+        </mask>
+        <path
+          d="M42.896 64.714H4.437A4.45 4.45 0 0 1 0 60.276v.37a4.45 4.45 0 0 0 4.438 4.437h38.458a4.45 4.45 0 0 0 4.437-4.437v-.37a4.45 4.45 0 0 1-4.437 4.438Z"
+          fillOpacity={0.2}
+          fill="#263238"
+          fillRule="nonzero"
+          mask="url(#m)"
+        />
+      </g>
+      <g transform="translate(.833 .958)">
+        <mask id="o" fill="#fff">
+          <use xlinkHref="#n" />
+        </mask>
+        <path
+          d="M34.02 17.75a4.436 4.436 0 0 1-4.437-4.438v.37a4.436 4.436 0 0 0 4.438 4.438h13.312v-.37H34.021Z"
+          fillOpacity={0.1}
+          fill="#263238"
+          fillRule="nonzero"
+          mask="url(#o)"
+        />
+      </g>
+      <path
+        d="M29.583 0H4.438A4.45 4.45 0 0 0 0 4.438v56.208a4.45 4.45 0 0 0 4.438 4.437h38.458a4.45 4.45 0 0 0 4.437-4.437V17.75L29.583 0Z"
+        fill="url(#p)"
+        fillRule="nonzero"
+        transform="translate(.833 .958)"
+      />
+    </g>
+  </svg>
+)
