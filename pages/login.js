8bd6a740bee115e1b67a207f481c1c397ea10e97
harley santos garzon <harley.santos.garzon@gmail.com>
2022-02-24 23:16:27 -0500

    feat(pages): add login

---
+++ pages/login.js
@@ -0,0 +1,48 @@
+import { useCallback, useRef, useState } from 'react';
+import Layout from '@components/layout';
+import { sheetPost } from '@services/index';
+
+const IData = { isAvailable: false, message: '', success: false };
+
+export default function LoginPage() {
+  const formRef = useRef(null);
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  });
+
+  const submitHandler = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      sheetPost(form).then((data = IData) => {
+        console.log('🤘 ~ getSheetInfo ~ sheetGet', data);
+        if (data.success && data.isAvailable) {
+          formRef.current.reset();
+        }
+      });
+    },
+    [form]
+  );
+  return (
+    <Layout title='Login'>
+      <form onSubmit={submitHandler} ref={formRef}>
+        <input
+          type={'email'}
+          name='email'
+          placeholder='Enter your email'
+          onChange={(event) => setForm({ ...form, email: event.target.value })}
+        />
+        <input
+          type={'password'}
+          name='password'
+          placeholder='Enter your password'
+          onChange={(event) =>
+            setForm({ ...form, password: event.target.value })
+          }
+        />
+        <button>submit</button>
+      </form>
+    </Layout>
+  );
+}
