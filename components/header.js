b0ab77bb870cbc2791512830baae66bba5d65015
harley santos garzon <harley.santos.garzon@gmail.com>
2022-02-28 22:49:30 -0500

    refactor(components): include Brand component and...
    validate CTA's according to session status

--- components/header.js
+++ components/header.js
@@ -1,23 +1,27 @@
 import Link from './navLink'
 import { signIn, signOut } from 'next-auth/react'
+import Brand from './brand'
 
 export default function Header({ session }) {
   return (
     <header className='o-header'>
       <div className='o-header_wrapper o-wrapper'>
+        <Brand size='2rem' />
         <nav className='o-header_nav'>
-          <Link activeClassName='is-active' href='/'>
-            <a className='o-header_link'>Home</a>
-          </Link>
           {session ? (
             <>
               <Link activeClassName='is-active' href='/adviser'>
                 <a className='o-header_link'>Adviser</a>
               </Link>
-              <button onClick={() => signOut()}>Sign out</button>
+              <button className='o-header_btn' onClick={() => signOut({callbackUrl: "/"})}>Sign out</button>
             </>
           ) : (
-            <button onClick={() => signIn()}>Sign in</button>
+            <>
+              <Link activeClassName='is-active' href='/'>
+                <a className='o-header_link'>Home</a>
+              </Link>
+              <button className='o-header_btn is-soft' onClick={() => signIn()}>Sign in</button>
+            </>
           )}
         </nav>
       </div>
