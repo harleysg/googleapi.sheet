aef33369a6de2621674fcec70aae0fc986b981a1
harley santos garzon <harley.santos.garzon@gmail.com>
2022-02-28 23:07:15 -0500

    refactor(pages): simplify index page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,22 @@
-import { useCallback } from 'react'
-import { useSession } from 'next-auth/react'
 
 import Layout from '@components/layout'
-import { sheetGet } from '@services/index'
-
-const IData = { data: {}, message: '', success: false, table: {} }
 
 export default function Home() {
-  const { data: session } = useSession()
-  const getSheetInfo = useCallback(() => {
-    session &&
-      sheetGet().then((data = IData) => {
-        console.log('🤘 ~ getSheetInfo ~ sheetGet', data);
-      })
-  }, [session])
 
   return (
     <Layout>
-      <h1>
-        Welcome to <a href='https://nextjs.org'>Next.js!</a>
-      </h1>
-      {session && <button onClick={getSheetInfo}>Get sheet info</button>}
+      <h1>Welcome to my GSheet</h1>
+      <h2>Instructions</h2>
+      <ul style={{listStyle: 'none'}}>
+        <li>◉ Click to Sign in button.</li>
+        <li>◉ Use your credentials to check the list relate to you.</li>
+      </ul>
+
+      <h3>TODO list</h3>
+      <ul style={{listStyle: 'none'}}>
+        <li>▢ Improve instructions</li>
+        <li>▢ Change brand image</li>
+      </ul>
     </Layout>
   );
 }
